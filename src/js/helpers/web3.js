27ff384c40d90db2ec55a333f973e5ea397098b8
Aaron Barnard <aaronbarnardsound@gmail.com>
2019-04-03 06:02:55 +1100

    added uuid for each transaction, general refactoring (#71)
    * added uuid for each transaction, general refactoring
    
    * update to version 0.5.1

--- src/js/helpers/web3.js
+++ src/js/helpers/web3.js
@@ -89,7 +89,9 @@ export const web3Functions = {
       default:
         return () => Promise.reject(errorObj)
     }
-  }
+  },
+  txReceipt: txHash =>
+    promisify(state.web3Instance.eth.getTransactionReceipt)(txHash)
 }
 
 export function configureWeb3(web3) {
@@ -167,10 +169,10 @@ export function getNonce(address) {
   return web3Functions.nonce(version)(address)
 }
 
-export function hasSufficientBalance(
+export function getTransactionParams(
   txObject = {},
   contractMethod,
-  methodArgs
+  contractEventObj
 ) {
   return new Promise(async (resolve, reject) => {
     const version = state.web3Version && state.web3Version.slice(0, 3)
@@ -180,7 +182,7 @@ export function hasSufficientBalance(
       txObject.value = formatNumber(txObject.value)
     }
 
-    const transactionValue = txObject.value
+    const value = txObject.value
       ? await web3Functions
           .bigNumber(version)(txObject.value)
           .catch(handleError('web3', reject))
@@ -206,7 +208,7 @@ export function hasSufficientBalance(
             await web3Functions
               .contractGas(version)(
                 contractMethod,
-                methodArgs.parameters,
+                contractEventObj.parameters,
                 txObject
               )
               .catch(handleError('web3', reject))
@@ -220,33 +222,32 @@ export function hasSufficientBalance(
           )
           .catch(handleError('web3', reject))
 
-    const transactionFee = gas.mul(gasPrice)
+    resolve({ value, gasPrice, gas })
+  })
+}
 
-    const buffer = transactionFee.div(
+export function hasSufficientBalance({ value, gas, gasPrice }) {
+  return new Promise(async (resolve, reject) => {
+    const version = state.web3Version && state.web3Version.slice(0, 3)
+
+    const gasCost = gas.mul(gasPrice)
+
+    const buffer = gasCost.div(
       await web3Functions
         .bigNumber(version)('10')
         .catch(handleError('web3', reject))
     )
 
-    const totalTransactionCost = transactionFee
-      .add(transactionValue)
-      .add(buffer)
+    const transactionCost = gasCost.add(value).add(buffer)
 
     const balance = await getAccountBalance().catch(handleError('web3', reject))
     const accountBalance = await web3Functions
       .bigNumber(version)(balance)
       .catch(handleError('web3', reject))
 
-    const sufficientBalance = accountBalance.gt(totalTransactionCost)
+    const sufficientBalance = accountBalance.gt(transactionCost)
 
-    const transactionParams = {
-      value: transactionValue.toString(),
-      gas: gas.toString(),
-      gasPrice: gasPrice.toString(),
-      to: txObject.to
-    }
-
-    resolve({ transactionParams, sufficientBalance })
+    resolve(sufficientBalance)
   })
 }
 
@@ -256,7 +257,7 @@ export function getAccountBalance() {
     updateState({ accountAddress: accounts[0] })
 
     const version = state.web3Version && state.web3Version.slice(0, 3)
-    const balance = web3Functions
+    const balance = await web3Functions
       .balance(version)(accounts[0])
       .catch(handleError('web3', reject))
 
@@ -318,19 +319,20 @@ export function getCurrentProvider() {
 
 // Poll for a tx receipt
 export function waitForTransactionReceipt(txHash) {
-  const web3 = state.web3Instance || window.web3
   return new Promise((resolve, reject) => {
+    return checkForReceipt()
+
     function checkForReceipt() {
-      web3.eth.getTransactionReceipt(txHash, (err, res) => {
-        if (err) {
-          return reject(err)
-        }
-        if (res === null) {
-          return setTimeout(() => checkForReceipt(), timeouts.pollForReceipt)
-        }
-        return resolve(res)
-      })
+      return web3Functions
+        .txReceipt(txHash)
+        .then(txReceipt => {
+          if (!txReceipt) {
+            return setTimeout(() => checkForReceipt(), timeouts.pollForReceipt)
+          }
+
+          return resolve(txReceipt)
+        })
+        .catch(reject)
     }
-    checkForReceipt()
   })
 }
