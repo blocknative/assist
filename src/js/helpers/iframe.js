d15085b26ae4ba88c8653436976cf5694786b816
Aaron Barnard <aaronbarnardsound@gmail.com>
2019-04-12 09:29:10 +1000

    Enhancement/custom css string (#88)
    * add ability to inject custom css string into iframe
    
    * refactored css so that font-family is set globally on the body for easy override
    
    * lowered transition lenght on iframe height
    
    * updated readme to include css option in config
    
    * update to version 0.5.3

--- src/js/helpers/iframe.js
+++ src/js/helpers/iframe.js
@@ -4,7 +4,7 @@ import { positionElement } from '../views/dom'
 import darkModeStyles from '../../css/dark-mode.css'
 
 export function createIframe(browserDocument, assistStyles, style = {}) {
-  const { darkMode } = style
+  const { darkMode, css } = style
 
   const initialIframeContent = `
     <html>
@@ -14,6 +14,9 @@ export function createIframe(browserDocument, assistStyles, style = {}) {
         </style>
         <style>
           ${darkMode ? darkModeStyles : ''}
+        </style>
+        <style>
+          ${css || ''}
         </style>
       </head>
       <body></body>
@@ -28,7 +31,7 @@ export function createIframe(browserDocument, assistStyles, style = {}) {
   iframe.style.border = 'none'
   iframe.style.pointerEvents = 'none'
   iframe.style.overflow = 'hidden'
-  iframe.style.transition = 'height 250ms ease-in-out'
+  iframe.style.transition = 'height 100ms ease-in-out'
   const iWindow = iframe.contentWindow
   const iDocument = iWindow.document
   iDocument.open()
