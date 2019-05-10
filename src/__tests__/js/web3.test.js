352b97bcb0f061712a4be2a65fff2d8c87e0a02a
Liam Aharon <liam.aharon@hotmail.com>
2019-05-10 12:36:33 +1000

    Create web3.js test structure

---
+++ src/__tests__/js/web3.test.js
@@ -0,0 +1,57 @@
+import da from '~/js'
+import { initialState, updateState } from '~/js/helpers/state'
+import * as websockets from '~/js/helpers/websockets'
+import { web3Functions } from '~/js/helpers/web3'
+
+const multidepRequire = require('multidep')('multidep.json')
+
+const initWeb3 = (simpleVersion, Web3) => {
+  if (simpleVersion === '1.0') {
+    return new Web3('ws://localhost:8546')
+  }
+  const provider = new Web3.providers.HttpProvider('http://localhost:8546')
+  return new Web3(provider)
+}
+
+describe(`web3.js tests`, () => {
+  multidepRequire.forEachVersion('web3', (version, Web3) => {
+    describe(`using web3 ${version}`, () => {
+      describe('assist is initialised correctly', () => {
+        let web3
+        let config
+        let simpleVersion
+        beforeEach(() => {
+          jest
+            .spyOn(websockets, 'openWebsocketConnection')
+            .mockImplementation(() => {})
+          simpleVersion = version.slice(0, 3)
+          web3 = initWeb3(simpleVersion, Web3)
+          config = { dappId: '123', web3, networkId: '5' }
+          da.init(config)
+        })
+        afterEach(() => {
+          // need to close any websocket connection
+          if (simpleVersion === '1.0') {
+            web3.currentProvider.connection.close()
+          }
+        })
+        describe('web3Functions', () => {
+          describe('networkId', () => {
+            test('should return the expected networkId', async () => {
+              const networkId = await web3Functions.networkId(simpleVersion)()
+              if (simpleVersion === '1.0') expect(networkId).toEqual(5)
+              else expect(networkId).toEqual('5') // 0.20 returns networkId as a string
+            })
+          })
+        })
+      })
+    })
+  })
+})
+
+afterEach(() => {
+  document.body.innerHTML = ''
+  updateState(initialState)
+  window.localStorage.clear()
+  jest.clearAllMocks()
+})
