25be58ece890885a0ebe668407878eeaf5e0d404
Liam Aharon <liam.aharon@hotmail.com>
2019-06-03 13:33:19 +1000

    Validate config on init (#235)
    * Ignore an uncaught rejection in test env
    
    * Mock websocket responses in init integration tests
    
    * Add missing networkId to test configs
    
    * Validate config on init
    
    * replace uglify with terser

--- internals/jestSetup.js
+++ internals/jestSetup.js
@@ -1,5 +1,6 @@
 // eslint-disable-next-line import/no-extraneous-dependencies
 import MockDate from 'mockdate'
+import { port as ganachePort } from './ganacheConfig'
 
 // Mock Date.now()
 MockDate.set('1/1/2010')
@@ -8,4 +9,19 @@ MockDate.set('1/1/2010')
 Object.defineProperty(window.navigator, 'userAgent', {
   value:
     'Mozilla/ 5.0(linux) AppleWebKit / 537.36(KHTML, like Gecko) jsdom / 11.12.0'
+})
+
+// Seemingly unavoidable uncaught rejections coming from the web3
+// websocket provider we setup to deploy the smart contracts, ignore them
+process.on('unhandledRejection', (reason, p) => {
+  if (
+    reason.target.constructor.name === 'W3CWebSocket' &&
+    reason.target.url === `ws://localhost:${ganachePort}` &&
+    reason.type === 'error'
+    // eslint-disable-next-line no-empty
+  ) {
+  } else {
+    // eslint-disable-next-line no-console
+    console.log('Unhandled Rejection at: ', p, 'reason:', reason)
+  }
 })
