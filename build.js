83bafb0303f40dcf58a019eaa213fcf48389ae5a
Liam Aharon <liam.aharon@hotmail.com>
2019-06-04 10:46:14 +1000

    - Manually specify es6 node_modules - Switch from terser to uglify

--- build.js
+++ build.js
@@ -6,7 +6,7 @@ const babel = require('rollup-plugin-babel')
 const { eslint } = require('rollup-plugin-eslint')
 const resolve = require('rollup-plugin-node-resolve')
 const commonjs = require('rollup-plugin-commonjs')
-const { terser } = require('rollup-plugin-terser')
+const { uglify } = require('rollup-plugin-uglify')
 const string = require('rollup-plugin-string')
 const json = require('rollup-plugin-json')
 const builtins = require('rollup-plugin-node-builtins')
@@ -40,27 +40,19 @@ const defaultPlugins = [
   builtins()
 ]
 
-// searches through node_modules and transfiles any non-es5 folders to es5
 function transpileEs5NodeModules() {
-  // get a list of es5 node_modules
-  const output = execSync('node_modules/.bin/are-you-es5 check -r .', {
-    encoding: 'utf8'
-  }).toString()
-  output
-    .split('\n') // get each line of the output
-    .filter(s => s.includes('is not ES5')) // filter the output for lines with non-es5 module names
-    // transform all non-es5 modules to es5
-    .forEach(s => {
-      const name = s.split(' ')[1] // module name is between the first two spaces
-      execSync(
-        `node_modules/.bin/babel node_modules/${name} --out-dir node_modules/${name} --presets=@babel/preset-env`
-      )
-    })
+  // specify any non-es5 modules here
+  const es5Modules = ['ow', 'punycode']
+  es5Modules.forEach(m =>
+    execSync(
+      `node_modules/.bin/babel node_modules/${m} --out-dir node_modules/${m} --presets=@babel/preset-env`
+    )
+  )
 }
 
 const inputOptions = min => ({
   input: 'src/js/index.js',
-  plugins: min ? [...defaultPlugins, terser()] : defaultPlugins
+  plugins: min ? [...defaultPlugins, uglify()] : defaultPlugins
 })
 
 const outputOptions = min => ({
